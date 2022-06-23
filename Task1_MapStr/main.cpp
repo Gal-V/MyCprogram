871f36f31d04a9701fa373d793f9da6d2ebf1bde
Gal-V <sir.vlad5451@yandex.ru>
2022-06-24 01:39:54 +0300

    ввод

--- Task1_MapStr/main.cpp
+++ Task1_MapStr/main.cpp
@@ -4,14 +4,26 @@
 
 using namespace std;
 
+struct access_s     //структура доступа
+{
+    string id;
+    map<string, string>* pointer;
+};
+
 int main()
 {
-    map<string, map<string, string>> external;
-    external["K"]["YID"] = "space1";// key key kontain
-    external["K"]["UAV"] = "space2";
-    external["K"]["RDV"] = "space3";
-    //external.emplace("K", make_pair("YID", "myItem1"));
-    //cout << external["value"] << endl;
+    map<string, map<string, string>> external;  //карта с картой
+    //external["K"]["YID"] = "space1";// [key | [key | kontain]]
+    external["K"]["YID"];   // first map
+    external["K"]["UAV"];
+    external["K"]["RDV"];
+    external["Z"]["YID"];   // second map
+    external["Z"]["UAV"];
+    external["Z"]["RDV"];
+    external["B"]["YID"]    ;// third map
+    external["B"]["UAV"];
+    external["B"]["RDV"];
+
     string key("K"); 
     auto it = external.find(key);
     if(it != external.end()) //проверка что эллемент существует
@@ -23,6 +35,9 @@ int main()
     {
         cout << "not find" << endl;
     }
+
+    access_s access[3] = {};
+
 
     return 0;
 }
