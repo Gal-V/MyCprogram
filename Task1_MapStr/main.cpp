03f9d6fc54350ed69b4923e5984aa4e787a8d934
Gal-V <sir.vlad5451@yandex.ru>
2022-06-27 15:13:47 +0300

    finish without rudiments

--- Task1_MapStr/main.cpp
+++ Task1_MapStr/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <map>
-//#include <array>
 
 using namespace std;
 
@@ -11,22 +10,45 @@ struct access_s     //структура доступа
     map<string, string>* pointer;
 };
 
-void fillMap(map<string, map<string, string>>* myMap, access_s* access) 
+//заполнение карты значениями 
+void fillMap(map<string, map<string, string>>* myMap, access_s* access);
+//вывод на экран карты
+void printMap(map<string, map<string, string>> myMap);
+
+
+int main()
 {
-    //string key1(access[0].id);
-    //string key2(access[1].id);
-    //string key3(access[2].id);
+    map<string, map<string, string>> external;  //карта с картой
+    //заполнение карт
+    external["K"]["YID"];   // first map
+    external["K"]["UAV"];
+    external["K"]["RDV"];
+    external["Z"]["YID"];   // second map
+    external["Z"]["UAV"];
+    external["Z"]["RDV"];
+    external["B"]["YID"];   // third map
+    external["B"]["UAV"];
+    external["B"]["RDV"];
+
     
-    //access[0].id = "K2";
-    //(*myMap)[key1]["YID"] = "K1";
-    /*(*myMap)[key1]["UAV"] = "K2";
-    (*myMap)[key1]["RDV"] = "K3";
-    (*myMap)[key2]["YID"] = "Z1";
-    (*myMap)[key2]["UAV"] = "Z2";
-    (*myMap)[key2]["RDV"] = "Z3";
-    (*myMap)[key3]["YID"] = "B1";
-    (*myMap)[key3]["UAV"] = "B2";
-    (*myMap)[key3]["RDV"] = "B3";*/
+    const int quantityMap(3);           //кол-во структур в массиве
+    access_s access[quantityMap] = {};  //массив структур
+
+    access[0].id = "YID";               //идентификаторы
+    access[1].id = "UAV";
+    access[2].id = "RDV";
+    access[0].pointer = &external["K"]; //адреса карт intern
+    access[1].pointer = &external["Z"];
+    access[2].pointer = &external["B"];
+
+    fillMap(&external, access);         //заполнение карт &access[quantityMap]
+    printMap(external);                 //вывод результатов
+
+    return 0;
+}
+
+void fillMap(map<string, map<string, string>>* myMap, access_s* access) 
+{   
     (*access[0].pointer)[access[0].id] = "K1";
     (*access[0].pointer)[access[1].id] = "K2";
     (*access[0].pointer)[access[2].id] = "K3";
@@ -41,7 +63,7 @@ void fillMap(map<string, map<string, string>>* myMap, access_s* access)
 
 void printMap(map<string, map<string, string>> myMap)
 {
-    for(const auto& it : myMap)   //вывод external key 
+    for(const auto& it : myMap)                  //вывод external key 
     {
         cout << it.first  << " ";
         cout << endl;
@@ -50,62 +72,5 @@ void printMap(map<string, map<string, string>> myMap)
             cout << itt.first <<  " " << itt.second <<  "\n";
         }
     }
-    //auto it = *myMap.find("K");
-    //cout << it->first <<endl; //key
-    //cout << (*myMap)["K"]["YID"] << endl;
-    /*
-    cout << (*access[0].pointer)[access[0].id] << "\t";
-    cout << (*access[0].pointer)[access[1].id] << "\t";
-    cout << (*access[0].pointer)[access[2].id] << endl;
-    cout << (*access[1].pointer)[access[0].id] << "\t";
-    cout << (*access[1].pointer)[access[1].id] << "\t";
-    cout << (*access[1].pointer)[access[2].id] << endl;
-    cout << (*access[2].pointer)[access[0].id] << "\t";
-    cout << (*access[2].pointer)[access[1].id] << "\t";
-    cout << (*access[2].pointer)[access[2].id] << endl;
-    */
-    /*string key("K"); 
-    auto it = external.find(key);
-    if(it != external.end()) //проверка что эллемент существует
-    {
-        cout << it->first <<endl; //key
-        //cout << it->second <<endl; //container
-    }else
-    {
-        cout << "not find" << endl;
-    }*/
-}
 
-int main()
-{
-    map<string, map<string, string>> external;  //карта с картой
-    //external["K"]["YID"] = "space1";// [key | [key | kontain]]
-    //заполнение карт
-    external["K"]["YID"];   // first map
-    external["K"]["UAV"];
-    external["K"]["RDV"];
-    external["Z"]["YID"];   // second map
-    external["Z"]["UAV"];
-    external["Z"]["RDV"];
-    external["B"]["YID"]    ;// third map
-    external["B"]["UAV"];
-    external["B"]["RDV"];
-
-    
-    const int quantityMap(3);           //кол-во структур в массиве
-    access_s access[quantityMap] = {};  //массив структур
-    //access[0].id = "K";                 //идентификаторы
-    //access[1].id = "Z";
-    //access[2].id = "B";
-    access[0].id = "YID";                 //идентификаторы
-    access[1].id = "UAV";
-    access[2].id = "RDV";
-    access[0].pointer = &external["K"]; //адреса карт intern
-    access[1].pointer = &external["Z"];
-    access[2].pointer = &external["B"];
-
-    fillMap(&external, access);         //заполнение карт &access[quantityMap]
-    printMap(external);                 //вывод результатов
-
-    return 0;
 }
