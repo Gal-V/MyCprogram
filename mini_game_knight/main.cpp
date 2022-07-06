0fb3d2f4bfa80870acffc35fc70fd669e3300cd8
Gal-V <sir.vlad5451@yandex.ru>
2022-07-06 13:09:48 +0300

    player inicialization

--- mini_game_knight/main.cpp
+++ mini_game_knight/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Creature
 {
+    protected:
     string m_name;
     char m_symbol;
     int m_health;
@@ -35,11 +36,33 @@ class Creature
     }
 };
 
+class Player: public Creature           //игрок
+{
+    int m_level = 1;
+
+    public:
+    Player(string p_name) : Creature(p_name, '@', 10, 1, 0) //начальные параметры
+    {}
+
+    int getLevel(){return m_level;}
+    void levelUp(){m_level++; m_damage++;}  //при левелапе +1 к дамагу
+    bool hasWon(){return (m_level == 20);}  //20 лвл = победа
+};
+
 int main(int argc, char const *argv[])
 {
+    string p_name("Player1");
+    cout << "Enter your name: ";
+    cin >> p_name;
+    cout << endl;
+    Player p(p_name);
+    cout << "Welcome, " << p.getName() << endl;
+    cout <<"You have " << p.getHealth() << " health and " << p.getGold() << " gold" << endl;
+
     Creature o("orc", 'o', 4, 2, 10);
     o.addGold(5);
     o.reduceHealth(1);
-    cout << o.getName() << " has " << o.getHealth() << " health and " << o.getGold() << " gold";
+    cout << o.getName() << " has " << o.getHealth() << " health and " << o.getGold() << " gold" << endl;
+
     return 0;
 }
